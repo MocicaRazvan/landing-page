afee925b63d17f86c3ea9d445f710d402b85502e
MocicaRazvan <razvanmocica@gmail.com>
2025-05-10 18:25:16 +0300

    added links

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -22,21 +22,30 @@ export default function Nav() {
       <div className="flex items-center justify-between w-full md:w-auto gap-5 md:gap-10">
         <div className="flex items-center justify-center gap-3 md:gap-7">
           <Link
-            href={process.env.NEXT_PUBLIC_BUISNESSFUNDATION_URL ?? "/"}
+            href={
+              process.env.NEXT_PUBLIC_BUISNESSFUNDATION_URL ??
+              "https://unibucro0-my.sharepoint.com/:w:/r/personal/andreea_creita_s_unibuc_ro/_layouts/15/doc2.aspx?sourcedoc=%7B732CD58D-77A9-444B-BB91-B7B224F9447C%7D&file=GymBroski.docx&action=default&mobileredirect=true&DefaultItemOpen=1"
+            }
             className="hover:underline hover:underline-offset-2 text-lg font-semibold text-primary hover:scale-[1.035] transition-all duration-150"
             target="_blank"
           >
             Buisness Fundation
           </Link>
           <Link
-            href={process.env.NEXT_PUBLIC_FIGMA_URL ?? "/"}
+            href={
+              process.env.NEXT_PUBLIC_FIGMA_URL ??
+              "https://www.figma.com/design/hCMtn8Iu74I5Jxg7ab4Jgw/Wireframes-UIUX?node-id=2047-773&t=e6eBauZCpIggJZcd-0"
+            }
             className="hover:underline hover:underline-offset-2 text-lg font-semibold text-primary hover:scale-[1.035] transition-all duration-150"
             target="_blank"
           >
             Figma
           </Link>
           <Link
-            href={process.env.NEXT_PUBLIC_PTICH_URL ?? "/"}
+            href={
+              process.env.NEXT_PUBLIC_PTICH_URL ??
+              "https://www.canva.com/design/DAGmx-bzTmI/IwPCSgCwRsiRokXac_24XQ/edit"
+            }
             className="hover:underline hover:underline-offset-2 text-lg font-semibold text-primary hover:scale-[1.035] transition-all duration-150"
             target="_blank"
           >
